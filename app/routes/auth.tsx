4e7a5719f6b72d6ea975fb44fed3867ec0225e7b
Prachi Sharma <ps8702222@gmail.com>
2026-03-03 21:59:58 +0530

    Authentication

---
+++ app/routes/auth.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+
+export const meta:() => any = () => ([
+    { title : 'Resumind | Auth'},
+    { name: 'description', content: 'Log into your account' },
+])
+const Auth = () => {
+      const { isLoading , auth} = usePuterStore();
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <div className="gradien-border shadow-lg">
+            <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+            <div className="flex flex-col items-center gap-2 text-center">
+                <h1>Welcome</h1>
+                <h2>Login In to Continue Your Job Journey</h2>
+            </div>
+                {isLoading ? (
+                    <button className="auth-button animate-pulse">
+                    <p>Signing you in...</p>
+                    </button>
+                ) : (
+                    <>
+                        {auth.isAuthenticated ? (
+                            <button className="auth-button" onClick={auth.signOut}>
+                                <p>Log Out</p>
+                            </button>
+                        ) : (
+                            <button className="auth-button" onClick={auth.signIn}>
+                                <p>Log In</p>
+                            </button>
+                        )}
+                    </>
+                )}
+             </section>
+        </div>
+        </main>
+    );
+};
+
+export default Auth;
